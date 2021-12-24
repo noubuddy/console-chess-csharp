55c69f1526ea4e65edfabbc59ecf202f1f89a80e
TiMobb <31501231+TiMobb@users.noreply.github.com>
2021-12-24 16:55:28 +0200

    initial commit

---
+++ ConsoleChessCSharp/Chess.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleChessCSharp
+{
+    public class Chess
+    {
+        private static readonly string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H" };
+        static readonly int size = 8;
+        private static bool[,]? chessboard;
+
+        public static void RenderBoard()
+        {
+            const string top = " ----------------------------------";
+            chessboard = new bool[size, size];
+
+            for (int i = 0; i < chessboard.GetLength(0); i++)
+            {
+                for (int j = 0; j < chessboard.GetLength(1); j++)
+                {
+                    chessboard[i, j] = true;
+                }
+            }
+            //chessboard[4, 6] = true;
+
+            for (int y = 0; y < size; y++)
+            {
+                Console.WriteLine(" " + top);
+                Console.Write(" " + (size - y));
+                for (int x = 0; x < size; x++)
+                {
+                    Console.Write("|" + (chessboard[x, y] ? " p " : "   "));
+                }
+                Console.WriteLine("|");
+            }
+            Console.Write("   ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(" " + letters[i] + "  ");
+            }
+        }
+
+
+
+    }
+}
---
+++ ConsoleChessCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleChessCSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.InputEncoding = Encoding.ASCII;
+            Console.OutputEncoding = Encoding.UTF8;
+            Chess.RenderBoard();
+            Console.ReadKey();
+        }
+    }
+}
