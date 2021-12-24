39aebb5a66eed8b48ece5356cae7582cf2d04444
TiMobb <31501231+TiMobb@users.noreply.github.com>
2021-12-24 23:44:48 +0200

    modified Main function

--- ConsoleChessCSharp/Program.cs
+++ ConsoleChessCSharp/Program.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ConsoleChessCSharp
+﻿namespace ConsoleChessCSharp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            //Console.InputEncoding = Encoding.ASCII;
-            Console.OutputEncoding = Encoding.UTF8;
-            Chess.RenderBoard();
+            Chess chess = new();
+            chess.InitBoard();
+
             Console.ReadKey();
         }
     }
